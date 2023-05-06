614b79ed02662d6c40b9bb84945b398d4a8c24e7
Agnija Bako <53393455+agnija-bako@users.noreply.github.com>
2023-05-06 10:33:50 +0200

    Add project files.

---
+++ WasteWiseFunctions/SensorProcessorFunction.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace WasteWiseFunctions
+{
+    public static class SensorFunction
+    {
+        [FunctionName("SensorProcessor")]
+        [return: ServiceBus("myqueue", Connection = "ServiceBusConnection")]
+        public static async Task<string> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "/sensor")] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            
+            return requestBody;
+        }
+    }
+}
